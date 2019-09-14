5702f360add07cc49a090274f203b30198686931
Supannee Mutitanon <billbillgm@gmail.com>
2019-09-14 23:17:30 +0700

    remove unuse

--- MuangUdonThaniMuseum/BeaconScanningManager.swift
+++
@@ -1,9 +0,0 @@
-//
-//  BeaconScanningManager.swift
-//  MuangUdonThaniMuseum
-//
-//  Created by Supannee Mutitanon on 14/9/19.
-//  Copyright © 2019 Supannee Mutitanon. All rights reserved.
-//
-
-import Foundation
